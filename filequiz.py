601048a7bc3c938939f249b68d46e2fdc72e046b
Kira <kira.anleu@hotmail.com>
2018-10-11 13:49:10 +0000

    Initialising the quiz game: Name, options and first input

---
+++ filequiz.py
@@ -0,0 +1,11 @@
+print("Quiz Game")
+print("**************************")
+print(" ")
+
+
+print("1. Run Quiz")
+print("2. Enter a Question")
+print("3. Exit")
+print(" ")
+
+option= input("Enter option: ")
