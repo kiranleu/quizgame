5114705cf101e0e4446cba2fc66c28be2c6695a1
Kira <kira.anleu@hotmail.com>
2018-10-11 15:08:59 +0000

    Added question function

--- filequiz.py
+++ filequiz.py
@@ -1,11 +1,37 @@
-print("Quiz Game")
-print("**************************")
-print(" ")
+def show_menu():
+    print("Quiz Game")
+    print("**************************")
+    print(" ")
 
 
-print("1. Run Quiz")
-print("2. Enter a Question")
-print("3. Exit")
-print(" ")
+    print("1. Run Quiz")
+    print("2. Enter a Question")
+    print("3. Exit")
+    print(" ")
 
-option= input("Enter option: ")
+    option= input("Enter option: ")
+
+    return option
+
+def add_a_question():
+     question = input("Enter a question: ")
+     answer = input ("You are very clever human! Tell me," + question + " I know you know it!")
+     
+     with open("questions.txt", "a") as f:
+         line = question + "|" + answer + "\n"
+         f.write(line)
+
+while True:
+    option = show_menu()
+    
+    if option == "1":
+        print("You picked to run the quiz")
+    if option == "2":
+        add_a_question()
+       
+    
+    if option == "3":
+      
+        break
+    
+    
